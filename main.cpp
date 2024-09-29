e93968028a4665ef693e85cde53c8d58d930c654
Abe <abenanduri1@gmail.com>
2024-09-29 12:59:02 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -8,3 +8,32 @@
 
 using namespace std; 
 
+class Movie
+{
+    private:
+
+        string title; 
+        string year; 
+        string writer; 
+
+    public: 
+        //setters
+        void setTitle(string t) { title = t; }
+        void setYear (string y) { year = y;}
+        void setWriter (string w) { writer = w; } 
+        //getters
+        string getTitle() { return title;}
+        string getYear() { return year; }
+        string getWriter() { return writer; }
+
+        void print()
+        {
+            cout<<"Movie: "<<title<<endl; 
+            cout<<"Year: "<<year<<endl; 
+            cout<<"Screenwriter: "<<writer<<endl; 
+
+        }
+
+
+};
+
